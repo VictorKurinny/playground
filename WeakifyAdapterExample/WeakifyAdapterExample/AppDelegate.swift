2056a99ff4b7764567a0a70a6070ffd1eaf5a9fc
Victor Kurinny <tyokman@gmail.com>
2020-04-28 19:27:05 +0300

    Add WeakifyAdapterExample project

---
+++ WeakifyAdapterExample/WeakifyAdapterExample/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  WeakifyAdapterExample
+//
+//  Created by Victor Kurinny on 28.04.2020.
+//  Copyright © 2020 VictorKurinny. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ WeakifyAdapterExample/WeakifyAdapterExample/ViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  WeakifyAdapterExample
+//
+//  Created by Victor Kurinny on 28.04.2020.
+//  Copyright © 2020 VictorKurinny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
