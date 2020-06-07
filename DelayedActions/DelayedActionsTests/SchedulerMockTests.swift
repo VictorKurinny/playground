4d945ab6230ab25c60fcabfdcae36cbed71011f5
Victor Kurinny <tyokman@gmail.com>
2020-06-07 16:00:11 +0300

    DelayedAction: Schedules actions by delay order

--- DelayedActions/DelayedActionsTests/SchedulerMockTests.swift
+++ DelayedActions/DelayedActionsTests/SchedulerMockTests.swift
@@ -17,11 +17,12 @@ final class SchedulerMock {
     func schedule(after delay: TimeInterval, block: @escaping () -> Void) {
         let action = Action(block: block, time: now + delay)
         actions.append(action)
+        actions.sort { $0.time < $1.time }
     }
 
     func timeTravel(by delay: TimeInterval) {
         now += delay
-        
+
         while let action = actions.first, action.time <= now {
             action.block()
             actions.removeFirst()
@@ -54,5 +55,22 @@ final class SchedulerMockTests: XCTestCase {
         XCTAssertTrue(events.isEmpty)
         sut.timeTravel(by: 2 * accuracy)
         XCTAssertEqual(events, [1])
+    }
+
+    func test_schedulesActionsByDelayOrder() {
+        let sut = SchedulerMock()
+        var events = [Int]()
+
+        sut.schedule(after: 5.0) {
+            events.append(2)
+        }
+
+        sut.schedule(after: 2.0) {
+            events.append(1)
+        }
+
+        sut.timeTravel(by: 6.0)
+
+        XCTAssertEqual(events, [1, 2])
     }
 }
