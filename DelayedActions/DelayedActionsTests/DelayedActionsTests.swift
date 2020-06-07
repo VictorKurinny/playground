353f1ce67cbe98996f85db1c433ae063faa1c432
Victor Kurinny <tyokman@gmail.com>
2020-06-07 15:18:45 +0300

    Create empty project (DelayedActions)

---
+++ DelayedActions/DelayedActionsTests/DelayedActionsTests.swift
@@ -0,0 +1,14 @@
+//
+//  DelayedActionsTests.swift
+//  DelayedActionsTests
+//
+//  Created by Victor Kurinny on 07.06.2020.
+//  Copyright © 2020 VictorKurinny. All rights reserved.
+//
+
+import XCTest
+@testable import DelayedActions
+
+class DelayedActionsTests: XCTestCase {
+
+}
